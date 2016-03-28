2a69ceecb841b474fe48f42dfba0acd61c93505e
rehan <ittsel.ali@devsinc.com>
2016-03-28 16:45:31 +0500

    rake button

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -1,3 +1,23 @@
-class StatusesController < InheritedResources::Base
-end
+require 'rake'
 
+Rake::Task.clear # necessary to avoid tasks being loaded several times in dev mode
+Academia::Application.load_tasks # providing your application name is 'sample'
+
+class StatusesController < InheritedResources::Base
+  def edit
+    flash[:warning] = "Test executed..."
+
+    # status = Status.find(params[:id])
+    url = Status.find(params[:id]).site_url
+
+    if url.include? 'cignaforhcp'
+      # Rake::Task["cigna_test"].reenable # in case you're going to invoke the same task second time.
+      Rake::Task["cigna_test"].invoke
+    elsif url.include? 'mhnet'
+      # Rake::Task["cigna_test"].reenable # in case you're going to invoke the same task second time.
+      Rake::Task["mhnet_test"].invoke
+    end
+
+    redirect_to :back
+  end
+end
