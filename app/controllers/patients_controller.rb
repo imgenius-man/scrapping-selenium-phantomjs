15b6d532ce2844c1f7ea25cab87e4099fa08843f
Ittsel Ali <ittsel.ali@devsinc.com>
2016-04-04 18:27:33 +0500

    as

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -116,8 +116,7 @@ class PatientsController < ApplicationController
    
     wait = Selenium::WebDriver::Wait.new(timeout: 20)
 
-    driver = Selenium::WebDriver.for :firefox
-    # , :args => ['--ignore-ssl-errors=true']
+    driver = Selenium::WebDriver.for :phantomjs, :args => ['--ignore-ssl-errors=true']
 
     driver.navigate.to site_url
 
