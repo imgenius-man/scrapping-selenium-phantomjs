9bb30f9bd4004a0fcbae125f3ef7699fb1e610b5
imgenius <im.geniusman@gmail.com>
2016-08-26 02:14:23 -0500

    fix eligibility

--- lib/parsers/parse_eligibility.rb
+++ lib/parsers/parse_eligibility.rb
@@ -3,6 +3,8 @@ require 'parsers/parse_table'
 class ParseEligibility
 
 	def get_coverage_json(current_array)		
+			
+		
 		headers = current_array['services'].map{|value| value['type_label']} if current_array['services'].present?
 		
 		json = {}
@@ -115,5 +117,78 @@ class ParseEligibility
 		}
 		 
 		 json
+		 
+	end
+
+
+	def get_medicare_json(current_array)
+		json = {}
+		# patient detail
+		patient_format = ParseTable.new.dummy_array_for_patient_detail
+		
+		patient_format['Patient Detail']['Last Name'] = current_array['last_name']
+		patient_format['Patient Detail']['First Name'] = current_array['first_name']
+		patient_format['Patient Detail']['Middle Name'] = current_array['middle_name']
+		patient_format['Patient Detail']['Patient ID'] = current_array['member_id']
+		patient_format['Patient Detail']['Group No.'] = current_array['group_id']
+		patient_format['Patient Detail']['Address 1'] = current_array['address']['street_line_1']
+		patient_format['Patient Detail']['Address 2'] = current_array['address']['street_line_2']
+		patient_format['Patient Detail']['City'] = current_array['address']['city']
+		patient_format['Patient Detail']['State'] = current_array['address']['state']
+		patient_format['Patient Detail']['Zip'] = current_array['address']['zip']
+		patient_format['Patient Detail']['Gender'] = current_array['gender']
+		patient_format['Patient Detail']['DOB'] = current_array['dob']
+
+
+		# plan_detail = current_array['plan_details'].present? ? current_array['plan_details'] : []
+		
+		# plan_detail.each { |plan|
+		# 	puts "---"*100
+		# 	puts plan.inspect
+		# 	patient_format['Plan and Network Detail']['Plan Type'] = plan['payer_name']
+		# 	patient_format['Plan and Network Detail']['Plan Type'] =  plan['payer_name'].to_s
+		# 	patient_format['Plan and Network Detail']['Initial Coverage Date'] = patient_format['Plan and Network Detail']['Initial Coverage Date'].to_s + plan['start_date'].to_s
+		# 	patient_format['Plan and Network Detail']['Current Coverage From'] = patient_format['Plan and Network Detail']['Current Coverage From'].to_s + plan['end_date'].to_s
+		# 	patient_format['Plan and Network Detail']['Account Name'] = patient_format['Plan and Network Detail']['Account Name'].to_s + plan['payer_name'].to_s
+		# }
+		
+
+		json = patient_format
+
+		(current_array['requested_service_types'].to_a+current_array['requested_procedure_codes'].to_a).each { |table|
+			dummy = ParseTable.new.dummy_array_for_tables_aetna
+
+			dummy['COPAY (PER VISIT)- IN NETWORK'] = table['copayment']
+			dummy['COINSURANCE (STANDARD)- IN NETWORK'] = table['coinsurance_percent']
+			dummy['INDIVIDUAL DEDUCTIBLE REMAINING - IN NETWORK'] = table['deductible_remaining']
+			dummy['INDIVIDUAL DEDUCTIBLE AMOUNT- IN NETWORK'] = table['deductible']
+
+			if table['type_label'].present?
+				dummy['CODE'] = table['type']
+				json.merge!({ table['type_label'] => dummy })
+
+			elsif table['procedure_label'].present?
+				dummy['CODE'] = table['procedure_code']
+				json.merge!({ table['procedure_label'] => dummy })
+			end
+					
+		}
+
+		json
 	end
 end
+
+# (current_array['requested_service_types']+current_array['requested_procedure_codes']).each { |table|
+# dummy = ParseTable.new.dummy_array_for_tables_aetna
+# dummy['COPAY (PER VISIT)- IN NETWORK'] = table['copayment']
+# dummy['COINSURANCE (STANDARD)- IN NETWORK'] = table['coinsurance_percent']
+# dummy['INDIVIDUAL DEDUCTIBLE REMAINING - IN NETWORK'] = table['deductible_remaining']
+# dummy['INDIVIDUAL DEDUCTIBLE AMOUNT- IN NETWORK'] = table['deductible']
+# if table['type_label'].present?
+# dummy['CODE'] = table['type']
+# json.merge!({ table['type_label'] => dummy })
+# elsif table['procedure_label'].present?
+# dummy['CODE'] = table['procedure_code']
+# json.merge!({ table['procedure_label'] => dummy })
+# end
+# }
